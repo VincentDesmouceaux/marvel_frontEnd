8f5b8a0eed7f8e8e52e71f38a76e6f9c7dd709e1
vincent desmouceaux <vincentdesmont@mbp-de-vincent.lan>
2022-11-17 17:27:57 +0100

    carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,9 @@ const Header = () => {
   return (
     <header className="header-container">
       <div>
-        <img className="header-logo" src={marvel} alt="marvel" />
-
+        <Link to={`/`}>
+          <img className="header-logo" src={marvel} alt="marvel" />
+        </Link>
         <Link to={`/comics`}>
           <h1>Comics</h1>
         </Link>
--- src/pages/CharactersId.js
+++ src/pages/CharactersId.js
@@ -6,6 +6,8 @@ import axios from "axios";
 const CharactersId = () => {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [data2, setData2] = useState();
+  const [isLoading2, setIsLoading2] = useState(true);
   const { id } = useParams();
 
   console.log(id);
@@ -13,21 +15,57 @@ const CharactersId = () => {
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get("http://localhost:3000/character/" + id);
-      //   setData(response.data);
+      setData(response.data);
       console.log(response.data);
-      //   setIsLoading(false);
+      setIsLoading(false);
     };
     fetchData();
+
+    const fetchData2 = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/comics/" + id);
+        setData2(response.data2);
+        setIsLoading2(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData2();
   }, [id]);
 
   return isLoading ? (
     <p>Loading ...</p>
   ) : (
-    <div className="offer-body">
-      {/* <img
-        className="offer-picture"
-        src={character.thumbnail.path + "." + character.thumbnail.extension}
-      /> */}
+    <div className="offer-container">
+      <div>
+        <img
+          className="imageC"
+          src={data.thumbnail.path + "." + data.thumbnail.extension}
+          alt="character"
+        />
+      </div>
+      <h1 className="nameC"> {data.name}</h1>
+      <div>
+        <h2>{data.description}</h2>
+      </div>
+
+      <div>
+        <div className="main">
+          {data.results.map((comics, index) => {
+            return (
+              <div className="card-container" key={comics._id}>
+                <h3>{comics.name}</h3>
+                <img
+                  src={comics.thumbnail.path + "." + comics.thumbnail.extension}
+                  alt="bande"
+                />
+
+                {comics.description && <p>{comics.description}</p>}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
